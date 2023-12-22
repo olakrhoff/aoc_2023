0ac25a32f268cdfb2668102729b1e75e912838af
olakrhoff <ola.kr@hotmail.no>
2023-12-23 00:59:04 +0100

    Part one of day twelve is done, a lot of rewriting to get it fast enough

---
+++ day_12/main.cpp
@@ -0,0 +1,251 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+#define DEBUG 0
+
+string trim(string line, char delimiter)
+{
+    while (line.at(0) == delimiter)
+        line = line.substr(1);
+    while (line.at(line.size() - 1) == delimiter)
+        line = line.substr(0, line.size() - 1);
+
+    return line;
+}
+
+vector<string> split(const string &line, const char delimiter)
+{
+    vector<string> res {};
+
+    string temp = line;
+    while (temp.find(delimiter) != -1)
+    {
+        res.emplace_back(temp.substr(0, temp.find(delimiter)));
+        temp = temp.substr(temp.find(delimiter) + 1);
+        temp = trim(temp, delimiter);
+    }
+    res.emplace_back(temp);
+
+    return res;
+}
+
+vector<uint64_t> get_numbers_from_list(const string &line)
+{
+    vector<uint64_t> nums {};
+
+    vector<string> string_nums = split(line, ',');
+
+    for (auto s: string_nums)
+        nums.emplace_back(stoll(s));
+
+    return nums;
+}
+
+typedef struct record
+{
+    string springs {};
+    vector<uint64_t> contiguous_springs {};
+
+    bool operator<(const record &rhs) const
+    {
+        if (springs < rhs.springs)
+            return true;
+        if (rhs.springs < springs)
+            return false;
+        return contiguous_springs < rhs.contiguous_springs;
+    }
+
+    bool operator>(const record &rhs) const
+    {
+        return rhs < *this;
+    }
+
+    bool operator<=(const record &rhs) const
+    {
+        return !(rhs < *this);
+    }
+
+    bool operator>=(const record &rhs) const
+    {
+        return !(*this < rhs);
+    }
+} record_t;
+
+record_t parse_record(const string &record)
+{
+    record_t rec;
+
+    rec.springs = record.substr(0, record.find(' '));
+    rec.contiguous_springs = get_numbers_from_list(record.substr(record.find(' ') + 1));
+
+    return rec;
+}
+
+bool valid(const record_t &record)
+{
+    int broken_expected {-1};
+    int broken_index = 0;
+    for (int i = 0; i < record.springs.size(); ++i)
+    {
+        if (broken_expected > 0 && record.springs.at(i) != '#')
+            return false;
+        if (broken_expected == 0 && record.springs.at(i) == '#')
+            return false;
+        if (broken_expected < 0 && record.springs.at(i) == '#')
+        {
+            if (broken_index >= record.contiguous_springs.size())
+                return false;
+            broken_expected = record.contiguous_springs.at(broken_index);
+            broken_index++;
+        }
+        broken_expected--;
+    }
+
+    if (broken_index < record.contiguous_springs.size() || broken_expected > 0)
+        return false;
+    return true;
+}
+
+ set<record_t> generate_permutations(const record_t &record)
+ {
+    set<record_t> perms {};
+    perms.insert(record);
+    set<record_t> temp {};
+
+    for (int group = 0; group < record.contiguous_springs.size(); ++group)
+    {
+        auto length = record.contiguous_springs.at(group);
+        for (auto per : perms)
+        {
+            // Firstly we want to account for the previous groups that they are in order
+            int index {};
+            for (int prev_group = 0; prev_group < group; ++prev_group)
+            {
+                auto len = record.contiguous_springs.at(prev_group);
+                for (int i = index; i < per.springs.length(); ++i)
+                {
+                    if (per.springs.at(i) == '#')
+                    {
+                        index = i + len + 1;
+                        break;
+                    }
+                }
+            }
+            for (int i = index; i < per.springs.length() - length + 1; ++i)
+            {
+                bool match = true;
+                int miss {-1};
+                for (int j = i; j < per.springs.length() && j < i + length; ++j)
+                {
+                    if (per.springs.at(j) == '.')
+                    {
+                        match = false;
+                        miss = j;
+                        break;
+                    }
+                }
+                if (!match)
+                {
+                    i = miss; // Skip as far ahead as you can
+                    continue;
+                }
+                if (i + length < per.springs.length() - 1)
+                    if (per.springs.at(i + length) == '#')
+                        continue;
+                if (i > 0 && per.springs.at(i - 1) == '#')
+                    continue;
+
+                record_t rec = per;
+                for (int j = i; j < per.springs.length() && j < i + length; ++j)
+                    rec.springs.at(j) = '#';
+
+                if (i + length < per.springs.length() - 1 && rec.springs.at(i + length) == '?')
+                    rec.springs.at(i + length) = '.';
+
+                for (int j = 0; j < i; ++j)
+                    if (rec.springs.at(j) == '?')
+                        rec.springs.at(j) = '.';
+
+                temp.insert(rec);
+            }
+        }
+        perms = temp;
+        temp = {};
+    }
+    for (auto per : perms)
+    {
+        replace(per.springs.begin(), per.springs.end(), '?', '.');
+        temp.insert(per);
+    }
+
+    return temp;
+ }
+
+uint64_t combinations(const record_t &record)
+{
+    set<record_t> permutations = generate_permutations(record);
+
+    uint64_t sum {};
+    for (auto per : permutations)
+        if (valid(per))
+            sum++;
+
+    return sum;
+}
+
+uint64_t find_combinations(const string &record)
+{
+    record_t rec = parse_record(record);
+
+    return combinations(rec);
+}
+
+uint64_t find_all_combinations(const vector<string> &records)
+{
+    uint64_t combinations {};
+
+    for (auto &record: records)
+        combinations += find_combinations(record);
+
+    return combinations;
+}
+
+int main()
+{
+    auto start_time = chrono::high_resolution_clock::now();
+    //Reading data from file
+    string filename = "../data.txt";
+    if (DEBUG)
+        filename = "../test_data.txt";
+    ifstream file;
+
+    file.open(filename);
+    if (!file.is_open())
+        return EXIT_FAILURE;
+
+    string temp {};
+    vector<string> records {};
+    while (!file.eof())
+    {
+        getline(file, temp);
+        records.emplace_back(temp);
+    }
+
+    file.close();
+
+    // Part 1
+    cout << "The number of combinations of springs are: " << find_all_combinations(records) << endl;
+
+    // Part 2
+
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+
+    cout << "Time taken: " << duration.count() / 1000 << " milliseconds" << endl;
+
+    return EXIT_SUCCESS;
+}
