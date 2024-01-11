ae7685bd4ed27d02e83872c3583ed442ede4dc35
olakristofferhoff <ola.kr@hotmail.no>
2024-01-11 14:24:20 +0100

    This was a low point, a bit of a let down, had to make a lot of assumptions to make it feasible

--- day_20/main.cpp
+++ day_20/main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 vector<string> split(const string &line, const string &delimiter)
 {
     vector<string> res {};
-
+    
     string temp = line;
     while (temp.find(delimiter) != -1)
     {
@@ -20,7 +20,7 @@ vector<string> split(const string &line, const string &delimiter)
         temp = temp.substr(temp.find(delimiter) + delimiter.length());
     }
     res.emplace_back(temp);
-
+    
     return res;
 }
 
@@ -34,7 +34,7 @@ typedef enum TYPE
 void parse_input(const string &line, map<string, tuple<vector<string>, TYPE_T, bool>> &adjacent)
 {
     vector<string> nodes {};
-
+    
     string from = line.substr(0, line.find(' '));
     TYPE_T type {NONE};
     if (from.at(0) == '%')
@@ -47,15 +47,15 @@ void parse_input(const string &line, map<string, tuple<vector<string>, TYPE_T, b
         type = CONJUNCTION;
         from = from.substr(1);
     }
-
+    
     string list = line.substr(line.find('>') + 2);
-
-    for (auto n : split(list, ", "))
+    
+    for (auto n: split(list, ", "))
         nodes.emplace_back(n);
-
+    
     if (!adjacent.contains(from))
         adjacent.insert({from, {}});
-    for (auto n : nodes)
+    for (auto n: nodes)
         get<0>(adjacent.at(from)).emplace_back(n);
     get<1>(adjacent.at(from)) = type;
     get<2>(adjacent.at(from)) = false;
@@ -68,7 +68,7 @@ pair<uint64_t, uint64_t> send_pulse(const string &start, map<string, tuple<vecto
     uint64_t high_pulse_counter {0};
     queue<tuple<string, bool, string>> modules {};
     modules.push({start, false, start});
-
+    
     while (!modules.empty())
     {
         string current;
@@ -76,14 +76,14 @@ pair<uint64_t, uint64_t> send_pulse(const string &start, map<string, tuple<vecto
         string from;
         tie(current, signal, from) = modules.front();
         modules.pop();
-
+        
         if (!adjacent.contains(current))
             continue;
-
+        
         switch (get<1>(adjacent.at(current)))
         {
             case NONE:
-                for (const auto &mod : get<0>(adjacent.at(current)))
+                for (const auto &mod: get<0>(adjacent.at(current)))
                 {
                     modules.push({mod, signal, current});
                     if (signal)
@@ -98,7 +98,7 @@ pair<uint64_t, uint64_t> send_pulse(const string &start, map<string, tuple<vecto
                     break;
                 bool &state = get<2>(adjacent.at(current));
                 state = !state;
-                for (const auto &mod : get<0>(adjacent.at(current)))
+                for (const auto &mod: get<0>(adjacent.at(current)))
                 {
                     modules.push({mod, state, current});
                     if (state)
@@ -109,20 +109,20 @@ pair<uint64_t, uint64_t> send_pulse(const string &start, map<string, tuple<vecto
                 break;
             }
             case CONJUNCTION:
-                for (auto &val : conjunction_map.at(current))
+                for (auto &val: conjunction_map.at(current))
                     if (val.first == from)
                         val.second = signal;
-
-
+                
+                
                 bool all_high {true};
-                for (const auto &val : conjunction_map.at(current))
+                for (const auto &val: conjunction_map.at(current))
                     if (!val.second)
                     {
                         all_high = false;
                         break;
                     }
-
-                for (const auto &val : get<0>(adjacent.at(current)))
+                
+                for (const auto &val: get<0>(adjacent.at(current)))
                 {
                     modules.push({val, !all_high, current});
                     if (!all_high)
@@ -142,14 +142,14 @@ uint64_t find_pulses_sent(map<string, tuple<vector<string>, TYPE_T, bool>> &adja
 {
     uint64_t low_pulse_count {};
     uint64_t high_pulse_count {};
-
+    
     for (int i = 0; i < iterations; ++i)
     {
         auto temp = send_pulse(start, adjacent, conjunction_map);
         low_pulse_count += temp.first;
         high_pulse_count += temp.second;
     }
-
+    
     return low_pulse_count * high_pulse_count;
 }
 
@@ -157,28 +157,28 @@ map<string, vector<pair<string, bool>>> create_conjunction_map(const map<string,
         tuple<vector<string>, TYPE_T, bool>> &adjacent)
 {
     map<string, vector<pair<string, bool>>> conjunction_map {};
-
-    for (auto [to, list] : adjacent)
+    
+    for (auto [to, list]: adjacent)
     {
-        for (const auto &from : get<0>(list))
+        for (const auto &from: get<0>(list))
         {
             if (!conjunction_map.contains(from))
                 conjunction_map.insert({from, {}});
             conjunction_map.at(from).emplace_back(to, false);
         }
     }
-
+    
     return conjunction_map;
 }
 
-vector<uint64_t> send_pulse_and_note(const string &start, const string &note,
-                                             map<string, tuple<vector<string>, TYPE_T, bool>> adjacent,
-                                             map<string, vector<pair<string, bool>>> conjunction_map)
+bool send_pulse_and_note(const string &start, const string &note,
+                         map<string, tuple<vector<string>, TYPE_T, bool>> &adjacent,
+                         map<string, vector<pair<string, bool>>> &conjunction_map,
+                         bool state)
 {
-    vector<uint64_t> low_signals {};
     queue<tuple<string, bool, string>> modules {};
     modules.push({start, false, start});
-
+    
     while (!modules.empty())
     {
         string current;
@@ -186,17 +186,17 @@ vector<uint64_t> send_pulse_and_note(const string &start, const string &note,
         string from;
         tie(current, signal, from) = modules.front();
         modules.pop();
-
+        
+        if (from == note && signal == state)
+            return true;
+        
         if (!adjacent.contains(current))
             continue;
-
-        if (from == note)
-            low_signals.emplace_back(1);
-
+        
         switch (get<1>(adjacent.at(current)))
         {
             case NONE:
-                for (const auto &mod : get<0>(adjacent.at(current)))
+                for (const auto &mod: get<0>(adjacent.at(current)))
                     modules.push({mod, signal, current});
                 break;
             case FLIP_FLOP:
@@ -205,39 +205,67 @@ vector<uint64_t> send_pulse_and_note(const string &start, const string &note,
                     break;
                 bool &state = get<2>(adjacent.at(current));
                 state = !state;
-                for (const auto &mod : get<0>(adjacent.at(current)))
+                for (const auto &mod: get<0>(adjacent.at(current)))
                     modules.push({mod, state, current});
                 break;
             }
             case CONJUNCTION:
-                for (auto &val : conjunction_map.at(current))
+                for (auto &val: conjunction_map.at(current))
                     if (val.first == from)
                         val.second = signal;
-
-
+                
+                
                 bool all_high {true};
-                for (const auto &val : conjunction_map.at(current))
+                for (const auto &val: conjunction_map.at(current))
                     if (!val.second)
                     {
                         all_high = false;
                         break;
                     }
-
-                for (const auto &val : get<0>(adjacent.at(current)))
+                
+                for (const auto &val: get<0>(adjacent.at(current)))
                     modules.push({val, !all_high, current});
                 break;
         }
     }
-    return low_signals;
+    return false;
+}
+
+uint64_t simulate_until_state(map<string, tuple<vector<string>, TYPE_T, bool>> adjacent,
+                              map<string, vector<pair<string, bool>>> conjunction_map,
+                              const string &current, bool state)
+{
+    for (int i = 1;; ++i)
+        if (send_pulse_and_note("broadcaster", current, adjacent, conjunction_map, state))
+            return i;
 }
 
 uint64_t find_min(map<string, tuple<vector<string>, TYPE_T, bool>> &adjacent,
                   map<string, vector<pair<string, bool>>> &conjunction_map,
                   const string &current, bool state)
 {
-    string test = "lf";
-
-    auto res = send_pulse_and_note("broadcaster", test, adjacent, conjunction_map);
+    if (get<1>(adjacent.at(current)) == CONJUNCTION)
+    {
+        bool only_conjunction_dependency = true;
+        for (auto [dep, _]: conjunction_map.at(current))
+            if (get<1>(adjacent.at(dep)) != CONJUNCTION)
+            {
+                only_conjunction_dependency = false;
+                break;
+            }
+        if (only_conjunction_dependency)
+        {
+            uint64_t sum {1};
+            for (auto [dep, _]: conjunction_map.at(current))
+                sum *= find_min(adjacent, conjunction_map, dep, !state);
+            return sum;
+        }
+        
+        return simulate_until_state(adjacent, conjunction_map, current, state);
+    }
+    
+    cout << "Should not be here, a BIG assumption though..." << endl;
+    exit(EXIT_FAILURE);
 }
 
 uint64_t find_min_steps(map<string, tuple<vector<string>, TYPE_T, bool>> &adjacent,
@@ -246,7 +274,7 @@ uint64_t find_min_steps(map<string, tuple<vector<string>, TYPE_T, bool>> &adjace
 {
     auto conjunction = conjunction_map.at(current);
     uint64_t min_steps = numeric_limits<uint64_t>::max();
-    for (auto [name, val] : conjunction)
+    for (auto [name, val]: conjunction)
     {
         auto steps = find_min(adjacent, conjunction_map, name, false);
         if (steps < min_steps)
@@ -261,13 +289,13 @@ void create_graph(const map<string, tuple<vector<string>, TYPE_T, bool>> &adjace
     file.open(filename);
     string start = "digraph G \n{\n";
     file.write(start.data(), start.length());
-    for (auto [name, val] : adjacent)
+    for (auto [name, val]: adjacent)
     {
         vector<string> list;
         TYPE_T type;
         bool state;
         tie(list, type, state) = val;
-
+        
         string temp = "\t";
         switch (type)
         {
@@ -281,7 +309,7 @@ void create_graph(const map<string, tuple<vector<string>, TYPE_T, bool>> &adjace
                 break;
         }
         temp += name + " -> ";
-
+        
         temp += "{";
         for (int i = 0; i < list.size(); ++i)
         {
@@ -306,11 +334,11 @@ int main()
     if (DEBUG)
         filename = "../test_data.txt";
     ifstream file;
-
+    
     file.open(filename);
     if (!file.is_open())
         return EXIT_FAILURE;
-
+    
     string temp {};
     map<string, tuple<vector<string>, TYPE_T, bool>> adjacent {};
     while (!file.eof())
@@ -318,11 +346,9 @@ int main()
         getline(file, temp);
         parse_input(temp, adjacent);
     }
-
+    
     file.close();
-
-    create_graph(adjacent, "../graph.gv");
-
+    
     auto conjunction_map = create_conjunction_map(adjacent);
     // Part 1
     if (PART_1)
@@ -331,11 +357,12 @@ int main()
     else
         cout << "Minimum steps to activate rx: " << find_min_steps(adjacent, conjunction_map, "rx")
              << endl;
-
+    
     auto end_time = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-
+    
     cout << "Time taken: " << duration.count() / 1000 << " milliseconds" << endl;
-
+    create_graph(adjacent, "../graph.gv");
+    
     return EXIT_SUCCESS;
 }
